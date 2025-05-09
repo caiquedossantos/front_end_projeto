88d2bd63408a15b946f58631912f69ec3d27a537
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-05-08 21:33:04 -0300

    Primeiro commit - lista de tarefas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import  logo from './logo.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      const [tarefa, setTarefa] = useState('');
+      const [tarefas, setTarefas] = useState([]);
+
+      const adicionarTarefa = () => {
+        if(tarefas.includes(tarefa)){
+          alert('Tarefa já existe na lista. ')
+          return;
+        }
+        setTarefas([...tarefas,tarefa]);
+
+        setTarefa('')
+      }
+
+      return(
+        <div className='App'>
+            <img src={logo} alt='Logo'/>
+            <hr/>
+            <h2>Adicionar Tarefa</h2>
+            <input
+            type='text'
+            placeholder='Adicione uma tarefa'
+            value={tarefa}
+            onChange={(e) => setTarefa(e.target.value)}
+            />
+            <button onClick={adicionarTarefa}>Adicionar</button>
+            <hr/>
+            <h2>Lista de Tarefas</h2>
+            <ol>
+              {tarefas.map((tarefa, index) => (<li key={index}>{tarefa}</li>))}
+            </ol>
+        </div>
+      );
+
 }
 
 export default App;
